323e07de294bf0d361f0c573d942219dbd2a0c0c
katsuhiro_yamashita <katsuhiro.yamashita.sr01@gmail.com>
2024-11-13 21:22:50 +0900

    Initial Commit

---
+++ TargetApp/TargetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TargetAppApp.swift
+//  TargetApp
+//
+//  Created by Apple on 2024/11/13.
+//
+
+import SwiftUI
+
+@main
+struct TargetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
