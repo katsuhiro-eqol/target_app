910f00638593c294b7f9386df27c969e6258bce3
katsuhiro_yamashita <katsuhiro.yamashita.sr01@gmail.com>
2024-11-14 09:31:46 +0900

    first commit

--- TargetApp/ContentView.swift
+++ TargetApp/ContentView.swift
@@ -8,16 +8,65 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var text = "new app"
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(text)
+            Spacer()
+            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                postData()
+            }
         }
         .padding()
     }
 }
+
+func changeText(text:String)->String{
+    if (text == "new app"){
+        return "second app"
+    } else {
+        return "new app"
+    }
+}
+
+func postData(){
+let testurl = URL(string: "https://next-line.onrender.com/api/cron")
+    let request = NSMutableURLRequest(url: testurl!)
+
+    // set the method(HTTP-POST)
+    request.httpMethod = "POST"
+    // set the header(s)
+    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+
+    // set the request-body(JSON)
+    let params: [String: Any] = [
+        "key": "cronToken",
+    ]
+//または以下のような記述
+//        var params = Dictionary<String,String>()
+//        params["testNumber"] = "123567"
+
+    do{
+        request.httpBody = try JSONSerialization.data(withJSONObject: params, options: [])
+    }catch{
+        print(error.localizedDescription)
+    }
+
+    // use NSURLSessionDataTask
+    let task = URLSession.shared.dataTask(with: request as URLRequest, completionHandler: {data, response, error in
+        if (error == nil) {
+            let result = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)!
+            print(result)
+        } else {
+            print(error as Any)
+        }
+    })
+    task.resume()
+}
+
 
 #Preview {
     ContentView()
